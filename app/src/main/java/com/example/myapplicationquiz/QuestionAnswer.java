3f7a7180453f4575688017529bacc47f7e7d7734
MegaMind1212 <borkaryash12@gmail.com>
2023-06-26 17:17:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationquiz/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.myapplicationquiz;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What is the analysis of digital data to uncover information for legal investigations called??",
+            "Which one is not the programming language?",
+            "Which intern from bharat intern has made this quiz?",
+            "Which company owns the Apple?"
+    };
+
+    public static String choices[][] = {
+            {"Display","Prevention","Examination.","Preservation"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Prince","Shreya","Yash","Not Yash"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Yash",
+            "Apple"
+    };
+}
